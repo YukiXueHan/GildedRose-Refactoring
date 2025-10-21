3f54d879c8c6ea2529d8fca9fa3160289dae19b9
Xue Han <xhan@alexandriatechnologies.onmicrosoft.com>
2025-10-20 20:32:42 -0700

    Refactor Gilded Rose using Strategy Pattern

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -10,38 +10,94 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
+class ItemUpdateStrategy:
+    
+    def update(self, item):
+        self._update_quality(item)
+        self._update_sell_in(item)
+        self._update_quality_after_sell_date(item)
+    
+    def _update_quality(self, item):
+        pass
+    
+    def _update_sell_in(self, item):
+        item.sell_in -= 1
+    
+    def _update_quality_after_sell_date(self, item):
+        pass
+    
+    def _increase_quality(self, item, amount=1):
+        item.quality = min(50, item.quality + amount)
+    
+    def _decrease_quality(self, item, amount=1):
+        item.quality = max(0, item.quality - amount)
+
+
+class NormalItemStrategy(ItemUpdateStrategy):
+    
+    def _update_quality(self, item):
+        self._decrease_quality(item)
+    
+    def _update_quality_after_sell_date(self, item):
+        if item.sell_in < 0:
+            self._decrease_quality(item)
+
+
+class AgedBrieStrategy(ItemUpdateStrategy):
+    
+    def _update_quality(self, item):
+        self._increase_quality(item)
+    
+    def _update_quality_after_sell_date(self, item):
+        if item.sell_in < 0:
+            self._increase_quality(item)
+
+
+class BackstagePassStrategy(ItemUpdateStrategy):
+    
+    def _update_quality(self, item):
+        self._increase_quality(item)
+        
+        if item.sell_in < 11:
+            self._increase_quality(item)
+        
+        if item.sell_in < 6:
+            self._increase_quality(item)
+    
+    def _update_quality_after_sell_date(self, item):
+        if item.sell_in < 0:
+            item.quality = 0
+
+
+class SulfurasStrategy(ItemUpdateStrategy):
+    
+    def _update_quality(self, item):
+        pass
+    
+    def _update_sell_in(self, item):
+        pass
+    
+    def _update_quality_after_sell_date(self, item):
+        pass
+
+
 class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
+        self.strategies = self._initialize_strategies()
+    
+    def _initialize_strategies(self):
+        return {
+            "Aged Brie": AgedBrieStrategy(),
+            "Backstage passes to a TAFKAL80ETC concert": BackstagePassStrategy(),
+            "Sulfuras, Hand of Ragnaros": SulfurasStrategy(),
+        }
+    
+    def _get_strategy(self, item):
+        return self.strategies.get(item.name, NormalItemStrategy())
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
+            strategy = self._get_strategy(item)
+            strategy.update(item)
